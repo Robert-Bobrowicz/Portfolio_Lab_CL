492bdf5b8bab9b6dd1d42881b188ce6e72724751
unknown <r.bobrowicz@live.com>
2021-09-28 18:18:02 +0200

    Home

--- portfolio_lab/src/components/Home_About_Us.js
+++ portfolio_lab/src/components/Home_About_Us.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomeAboutUs = () => {
     return (
         <div className="aboutUs" id="aboutUs">
-            <img src="../assets/People.jpg" />
+            <img src="../assets/People.jpg" alt="people"/>
             <div className="about_us"> O nas </div>
         </div>
     );
--- portfolio_lab/src/components/Home_Navigation_Header.js
+++ portfolio_lab/src/components/Home_Navigation_Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {Link} from 'react-scroll';
+
 
 const HomeNavigationHeader = () => {
     return (
         <div className="navigation_header" id="navigation_header_ID">
             <ul className="login" id="login">
-                <li>Zaloguj</li>
-                <li>Załóż konto</li>
+                <li><Link to="/log_in">Zaloguj</Link></li>
+                <li><Link to="/register">Załóż konto</Link></li>
             </ul>
             <ul className="nav_menu" id="nav_menu">
-                <li><a href="#homePage">Start</a></li>
+                <li><Link to="#mainPage">Start</Link></li>
                 <li><a href="#threeColumns">O co chodzi?</a></li>
                 <li><a href="#aboutUs">O nas</a></li>
                 <li><a href="#foundations">Fundacja i organizacje</a></li>
