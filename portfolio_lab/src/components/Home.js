82ec52f0f9dded54b032f6529cd5359157f28fa3
unknown <r.bobrowicz@live.com>
2021-09-28 16:54:27 +0200

    main page

--- portfolio_lab/src/components/Home.js
+++ portfolio_lab/src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Link} from 'react-scroll';
+
 import HomeNavigationHeader from "./Home_Navigation_Header";
-import HomePage from "./Home_Page";
+import HomeMainPage from "./Home_Main_Page";
 import HomeThreeColumns from "./Home_Three_columns";
 import HomeFourSteps from "./Home_Four_Steps";
 import HomeAboutUs from "./Home_About_Us";
@@ -11,7 +13,7 @@ const Home = () => {
     return (
         <div>
             <HomeNavigationHeader />
-            <HomePage />
+            <HomeMainPage />
             <HomeThreeColumns />
             <HomeFourSteps />
             <HomeAboutUs />
--- portfolio_lab/src/components/Home_Main_Page.js
+++ portfolio_lab/src/components/Home_Main_Page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HomePage = () => {
+const HomeMainPage = () => {
     return (
         <div className="homePage" id="homePage">
             <img src="../assets/Home-Hero-Image.jpg" alt="Home hero"/>
@@ -9,12 +9,12 @@ const HomePage = () => {
                 <h1>Oddaj niechciane rzeczy w zaufane ręce</h1>
                 <img src="../assets/Decoration.svg" alt="decoration"/>
                 <div>
-                    <a href="#">Oddaj rzeczy</a>
-                    <a href="#">Zorganizuj zbiórkę</a>
+                    <a href="#fourSteps">Oddaj rzeczy</a>
+                    <a href="#fourSteps">Zorganizuj zbiórkę</a>
                 </div>
             </div>
         </div>
     );
 };
 
-export default HomePage;
+export default HomeMainPage;
