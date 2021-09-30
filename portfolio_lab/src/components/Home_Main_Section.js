e66294f7db639e349c4d6939e895c9d56859da73
unknown <r.bobrowicz@live.com>
2021-09-30 09:03:51 +0200

    home main section style

--- portfolio_lab/src/components/Home_Main_Section.js
+++ portfolio_lab/src/components/Home_Main_Section.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
 import {Link as ScrollLink} from 'react-scroll';
 import homeHeroImage from "../assets/Home-Hero-Image.jpg";
 import decoration from "../assets/Decoration.svg";
 
 const HomeMainSection = () => {
     return (
-        <div className="mainSection" id="mainSection">
-            <img src={homeHeroImage} alt="Home hero"/>
-            <div>
-                <h1>Zacznij pomagać</h1>
-                <h1>Oddaj niechciane rzeczy w zaufane ręce</h1>
-                <img src={decoration} alt="decoration"/>
-                <div>
-                    <ScrollLink to="fourSteps">Oddaj rzeczy</ScrollLink>
-                    <ScrollLink to="fourSteps">Zorganizuj zbiórkę</ScrollLink>
-                </div>
-            </div>
-        </div>
+        <Container fluid className="mainSection" id="mainSection">
+            <Row>
+                <Col>
+                    <div className="mainSectionImage">
+                        <img src={homeHeroImage} alt="Home hero" className="homeHeroImage"/>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="mainSectionText">
+                        <h3>Zacznij pomagać</h3>
+                        <h3>Oddaj niechciane rzeczy w zaufane ręce</h3>
+                        <img src={decoration} alt="decoration"/>
+                        <div className="mainSectionButtons">
+                            <button>
+                                <ScrollLink to="fourSteps">Oddaj rzeczy</ScrollLink>
+                            </button>
+                            <button>
+                                <ScrollLink to="fourSteps">Zorganizuj zbiórkę</ScrollLink>
+                            </button>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
--- portfolio_lab/src/components/Test.js
+++ portfolio_lab/src/components/Test.js
@@ -3,7 +3,13 @@ import {Container, Row, Col} from "react-bootstrap";
 
 const Test = () => {
     return (
-            <Container>
+            <Container fluid>
+                <Row>
+                    <Col>1 of 4</Col>
+                    <Col>2 of 4</Col>
+                    <Col>1 of 1</Col>
+                    <Col>1 of 1</Col>
+                </Row>
                 <Row>
                     <Col>1 of 4</Col>
                     <Col>2 of 4</Col>
