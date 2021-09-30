9fb8dd4443c9f7dbb12074e23243d50c8b903901
unknown <r.bobrowicz@live.com>
2021-09-30 10:01:15 +0200

    three coulmns section

--- portfolio_lab/src/components/Home_Three_columns.js
+++ portfolio_lab/src/components/Home_Three_columns.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
 
 const HomeThreeColumns = () => {
     return (
-        <div className="threeColumns" id="threeColumns">
-            <div className="columnOne">
-                <p>10</p>
-            </div>
-            <div className="columnTwo">
-                <p>5</p>
-            </div>
-            <div className="columnThree">
-                <p>7</p>
-            </div>
-        </div>
+        <Container fluid className="threeColumns">
+            <Row>
+                <Col>
+                    <div className="columnOne column">
+                        <p>10</p>
+                        <p>Oddanych worków</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="columnTwo column">
+                        <p>5</p>
+                        <p>Wspartych organizacji</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="columnThree column">
+                        <p>7</p>
+                        <p>Zorganizowanych zbiórek</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
