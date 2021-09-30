9b246873a02a4036eab2caf6d5fa1d3c5c97e1f9
unknown <r.bobrowicz@live.com>
2021-09-30 12:42:33 +0200

    about us style

--- portfolio_lab/src/components/Home_About_Us.js
+++ portfolio_lab/src/components/Home_About_Us.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
 import people from "../assets/People.jpg";
+import decoration from "../assets/Decoration.svg";
+import podpis from "../assets/Signature.svg";
 
 const HomeAboutUs = () => {
     return (
-        <div className="aboutUs" id="aboutUs">
-            <img src={people} alt="people"/>
-            <div className="about_us"> O nas </div>
-        </div>
+        <Container fluid className="aboutUs" id="aboutUs">
+            <Row>
+                <Col>
+                    <h3>O nas</h3>
+                    <img src={decoration} alt="decoration" />
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    <img src={podpis} alt="podpis prezesa" />
+                </Col>
+                <Col>
+                    <img src={people} alt="people"/>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
