23448cc2eed79e30eb16c4624d23f70fd7f7c0b0
unknown <r.bobrowicz@live.com>
2021-09-30 17:44:19 +0200

    who we help style

--- portfolio_lab/src/components/Home_Foundations.js
+++ portfolio_lab/src/components/Home_Foundations.js
@@ -4,9 +4,9 @@ import {Container, Row, Col} from "react-bootstrap";
 
 const HomeFoundations = () => {
     return (
-        <Container className="whoWeHelp" id="whoWeHelp">
+        <Container>
             <Row>
-                <Col>
+                <Col className="whoWeHelp" id="whoWeHelp">
                     <h2>Komu pomagamy</h2>
                     <img src={decoration} alt="decoration line"/>
                     <ul>
@@ -14,7 +14,7 @@ const HomeFoundations = () => {
                         <li>Organizacjom pozarządowym</li>
                         <li>Lokalnym zbiorkom</li>
                     </ul>
-                    <p>Lorem ipsum .... </p>
+                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
                 </Col>
             </Row>
         </Container>
