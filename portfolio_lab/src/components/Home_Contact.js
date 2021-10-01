dc3d5765f55aef8f8045229130bf80bf8bd7fad5
unknown <r.bobrowicz@live.com>
2021-10-01 08:40:28 +0200

    contact style

--- portfolio_lab/src/components/Home_Contact.js
+++ portfolio_lab/src/components/Home_Contact.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import backgroundContactForm from "../assets/Background-Contact-Form.jpg";
+import decoration from "../assets/Decoration.svg";
+import facebookIcon from "../assets/Facebook.svg";
+import instagramIcon from "../assets/Instagram.svg";
 
 const HomeContact = () => {
     return (
-        <div className="contact" id="contact">
-            <form>
-                <label>Kontakt</label>
-                <input type="text"></input>
-            </form>
-        </div>
+        <Container className="contact" id="contact">
+            <Row>
+                <Col>
+                    <img src={backgroundContactForm} alt="background" />
+                </Col>
+                <Col>
+                    <h3>Skontaktuj się z nami</h3>
+                    <img src={decoration} alt="decoration" />
+                    <form>
+                        <div>
+                            <div>
+                                <label>Wpisz swoje imię</label>
+                                <input type="text" placeholder="wpisz swoje imię"/>
+                            </div>
+                            <div>
+                                <label>Wpisz swój email</label>
+                                <input type="text" placeholder="wpisz swój email"/>
+                            </div>
+                        </div>
+                        <div>
+                            <label>Wpisz swoją wiadomość</label>
+                            <input type="textarea"/>
+                        </div>
+                        <div>
+                            <button>Wyślij</button>
+                        </div>
+                    </form>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>Copyright by Coders Lab</p>
+                    <img src={facebookIcon} alt="facebook" />
+                    <img src={instagramIcon} alt="instagram" />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
