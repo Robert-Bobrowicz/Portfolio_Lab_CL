f3d78b5af305fd49130ebbc9fafb212ef5ccc821
unknown <r.bobrowicz@live.com>
2021-10-02 10:29:10 +0200

    databases

---
+++ portfolio_lab/src/components/data/foundationsDB.js
@@ -0,0 +1,56 @@
+export const foundationsData = [
+    {
+        id: 1,
+        name: "Dbam o Zdrowie",
+        description: "Pomóc osobom znajdującym sie  w trudnej sytuacji życiowej.",
+        whatWeCollect: "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+    },
+    {
+        id: 2,
+        name: "Dla dzieci",
+        description: "Pomóc dzieciom z ubogich rodzin.",
+        whatWeCollect: "ubrania, meble, zabawki"
+    },
+    {
+        id: 3,
+        name: "Bez domu",
+        description: "Pomoc dla osób nieposiadających miejsca zamieszkania.",
+        whatWeCollect: "ubrania, jedzenie, ciepłe koce"
+    },
+    {
+        id: 4,
+        name: "Dbam o Życie",
+        description: "Pomóc osobom znajdującym sie  w trudnej sytuacji życiowej.",
+        whatWeCollect: "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+    },
+    {
+        id: 5,
+        name: "Zdążyć z Pomocą",
+        description: "Pomóc rodzinom z niepełnosprawnymi dziećmi.",
+        whatWeCollect: "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+    },
+    {
+        id: 6,
+        name: "Mamo idę na boisko",
+        description: "Pomóc w rozwoju sportowym dzieci z ubogich rodzin.",
+        whatWeCollect: "ubrania i sprzęt sportowy, oferty zajęć sportowych, zabawki"
+    },
+    {
+        id: 7,
+        name: "Już pływam",
+        description: "Nieodpłatna nauka pływania dla dzieci z ubogich rodzin.",
+        whatWeCollect: "oferty zajęć nauki pływania, sprzęt sportowy do pływania, zabawki"
+    },
+    {
+        id: 8,
+        name: "Marzenia o lataniu",
+        description: "Nauka latania dla młodzieży powyżej 14 r.ż. z rodzin znajdujących się  w trudnej sytuacji życiowej.",
+        whatWeCollect: "oferty zajęć nauki pilotażu szybowcami, dronami oraz nauki skoków spadochronowych"
+    },
+    {
+        id: 9,
+        name: "Już kieruję",
+        description: "Kursy prawa jazdy kat. B dla młodzieży z rodzin znajdujących się  w trudnej sytuacji życiowej.",
+        whatWeCollect: "oferty szkół nauki jazdy, oferty sponsorów dotyczące finansowania nauki jazdy"
+    }
+]
---
+++ portfolio_lab/src/components/data/organisationsDB.js
@@ -0,0 +1,38 @@
+export const organisationsData = [
+    {
+        id: 1,
+        name: "Lorem Ipsum 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        whatWeCollect: "lorem ipsum, amet, dolor"
+    },
+    {
+        id: 2,
+        name: "Lorem Ipsum 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        whatWeCollect: "lorem ipsum, amet, dolor"
+    },
+    {
+        id: 3,
+        name: "Lorem Ipsum 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        whatWeCollect: "lorem ipsum, amet, dolor"
+    },
+    {
+        id: 4,
+        name: "Lorem Ipsum 4",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        whatWeCollect: "lorem ipsum, amet, dolor"
+    },
+    {
+        id: 5,
+        name: "Lorem Ipsum 5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        whatWeCollect: "lorem ipsum, amet, dolor"
+    },
+    {
+        id: 6,
+        name: "Lorem Ipsum 6",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        whatWeCollect: "lorem ipsum, amet, dolor"
+    }
+]
