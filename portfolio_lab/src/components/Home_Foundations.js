ce0b8c044324a31baee8d4821933f758db018c74
unknown <r.bobrowicz@live.com>
2021-10-03 08:12:53 +0200

    foundations data lists

--- portfolio_lab/src/components/Home_Foundations.js
+++ portfolio_lab/src/components/Home_Foundations.js
@@ -34,8 +34,7 @@ const HomeFoundations = () => {
             </Row>
             <Row>
                 <Col className="foundationsList">
-                    <p>Sprawdzone fundacje, które wspieramy:</p>
-                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
+                    <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, którym pomagamy i z którymi współpracujemy. Możesz sprawdzić czym sie zajmują, komu pomagają i czego potrzebują ich podopieczni.</p>
                     <ul className="list-group mb-4">
                         {foundationsData.map((el) => (
                             <li key={el.id} className="list-group-item mb-2">
@@ -47,7 +46,6 @@ const HomeFoundations = () => {
             </Row>
             <Row>
                 <Col className="organisationsList">
-                    <p>Sprawdzone fundacje, które wspieramy:</p>
                     <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
                     <ul className="list-group mb-4">
                         {organisationsData.map((el) => (
@@ -60,8 +58,7 @@ const HomeFoundations = () => {
             </Row>
             <Row>
                 <Col className="localEventsList">
-                    <p>Sprawdzone fundacje, które wspieramy:</p>
-                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
+                    <p className="mb-4">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
                     <ul className="list-group mb-4">
                         {localEventsData.map((el) => (
                             <li key={el.id} className="list-group-item mb-2">
