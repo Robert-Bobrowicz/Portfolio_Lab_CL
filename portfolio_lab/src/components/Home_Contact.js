e8a7f4a5d605867e8ee33f7901ae8d11ab6b1d90
unknown <r.bobrowicz@live.com>
2021-10-05 09:51:29 +0200

    contact form

--- portfolio_lab/src/components/Home_Contact.js
+++ portfolio_lab/src/components/Home_Contact.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Row, Col} from "react-bootstrap";
 import decoration from "../assets/Decoration.svg";
 import facebookIcon from "../assets/Facebook.svg";
 import instagramIcon from "../assets/Instagram.svg";
 
 const HomeContact = () => {
+    const [errorName, setErrorName] = useState('');
+    const [errorEmail, setErrorEmail] = useState('');
+    const [errorTextarea, setErrorTextarea] = useState('');
+    const [submitSuccess, setSubmitSuccess] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,20 +22,24 @@ const HomeContact = () => {
                     <Col className="contactForm">
                         <h4>Skontaktuj się z nami</h4>
                         <img src={decoration} alt="decoration" />
+                        <p className="submitSuccess">{submitSuccess}</p>
                         <form>
                             <div className="contactName_Email">
                                 <div className="contactName">
                                     <label>Wpisz swoje imię</label>
                                     <input type="text" placeholder="wpisz swoje imię"/>
+                                    <p className="nameError">{errorName}</p>
                                 </div>
                                 <div className="contactEmail">
                                     <label>Wpisz swój email</label>
-                                    <input type="text" placeholder="wpisz swój email"/>
+                                    <input type="email" placeholder="wpisz swój email"/>
+                                    <p className="emailError">{errorEmail}</p>
                                 </div>
                             </div>
                             <div className="contactTextArea">
                                 <label>Wpisz swoją wiadomość</label>
                                 <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." cols="30" rows="5"/>
+                                <p className="textAreaError">{errorTextarea}</p>
                             </div>
                             <div className="button">
                                 <button onClick={handleSubmit}>Wyślij</button>
