e03b49dd862f08c76d85ca6bb00e33abb1ab536c
unknown <r.bobrowicz@live.com>
2021-10-07 10:38:57 +0200

    register panel

--- portfolio_lab/src/components/Register.js
+++ portfolio_lab/src/components/Register.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import {Container} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import decoration from "../assets/Decoration.svg";
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <Container fluid className="register">
-            Zarejestruj się
+        <Container fluid className="userLogin">
+            <Row>
+                <Col className="loginPanel">
+                    <h3>Zaloguj się</h3>
+                    <img src={decoration} alt="decoration" />
+                    <form className="loginForm">
+                        <div>
+                            <div className="loginEmail">
+                                <label className="mb-2">Email</label>
+                                <input type="email" className="mb-2" onChange={handleEmail}/>
+                            </div>
+                            <div className="loginPassword">
+                                <label className="mb-2">Hasło</label>
+                                <input type="password" className="mb-2" onChange={handlePassword}/>
+                            </div>
+                            <div className="loginPassword">
+                                <label className="mb-2">Powtórz hasło</label>
+                                <input type="password" className="mb-2" onChange={handlePassword}/>
+                            </div>
+                        </div>
+                        <div className="loginButtons">
+                            <button className="btn mx-2">Załóż konto</button>
+                            <button type="submit" className="btn submit mx-2" onSubmit={handleSubmit}>Zaloguj się</button>
+                        </div>
+                    </form>
+                </Col>
+            </Row>
         </Container>
     );
 };
