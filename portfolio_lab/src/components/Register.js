c335cba86032ab404eb9610062eed2dbff6d4ce9
unknown <r.bobrowicz@live.com>
2021-10-09 08:35:54 +0200

    validation

--- portfolio_lab/src/components/Register.js
+++ portfolio_lab/src/components/Register.js
@@ -6,6 +6,10 @@ import HomeNavigationHeader from "./Home_Navigation_Header";
 const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordRepeat, setPasswordRepeat] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [passwordRepeatError, setPasswordRepeatError] = useState('');
 
     const handleEmail = (e) => {
         setEmail(e.target.value);
@@ -15,8 +19,43 @@ const Register = () => {
         setPassword(e.target.value);
     }
 
+    const handlePasswordRepeat = (e) => {
+        setPassword(e.target.value);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (!email.includes('@') || email === '') {
+            console.log('email does not includes @ or is empty string');
+            setEmailError('Nieprawidłowy email');
+        } else {
+            setEmailError('');
+        }
+
+        if (password <= 6) {
+            console.log('password is less than 6 characters');
+            setPasswordError('Hasło musi zawierać co najmniej 6 znaków');
+        } else {
+            setPasswordError('');
+        }
+
+        if (passwordRepeat !== password) {
+            console.log('passwords are not equal');
+            setPasswordRepeatError('Hasła nie są takie same');
+        } else {
+            setPasswordRepeatError('');
+        }
+
+        if ( (email !== '' && email.includes('@')) && (password.length >= 6) && (password === passwordRepeat)) {
+            clearBox();
+        }
+    }
+
+    const clearBox = () => {
+        setEmail('');
+        setPassword('');
+        setPasswordRepeat('');
     }
 
     return (
@@ -34,24 +73,39 @@ const Register = () => {
                     <Col className="loginPanel">
                         <h3>Zaloguj się</h3>
                         <img src={decoration} alt="decoration" />
-                        <form className="loginForm">
+                        <form className="loginForm" onSubmit={handleSubmit}>
                             <div>
                                 <div className="loginEmail">
                                     <label className="mb-2">Email</label>
-                                    <input type="email" className="mb-2" onChange={handleEmail}/>
+                                    <input
+                                        type="text"
+                                        className="mb-2"
+                                        onChange={handleEmail}
+                                        value={email}/>
+                                    <p>{emailError}</p>
                                 </div>
                                 <div className="loginPassword">
                                     <label className="mb-2">Hasło</label>
-                                    <input type="password" className="mb-2" onChange={handlePassword}/>
+                                    <input
+                                        type="password"
+                                        className="mb-2"
+                                        onChange={handlePassword}
+                                        value={password}/>
+                                    <p>{passwordError}</p>
                                 </div>
-                                <div className="loginPassword">
+                                <div className="loginPasswordRepeat">
                                     <label className="mb-2">Powtórz hasło</label>
-                                    <input type="password" className="mb-2" onChange={handlePassword}/>
+                                    <input
+                                        type="password"
+                                        className="mb-2"
+                                        onChange={handlePasswordRepeat}
+                                        value={passwordRepeat}/>
+                                    <p>{passwordRepeatError}</p>
                                 </div>
                             </div>
                             <div className="loginButtons">
                                 <button className="btn mx-2">Załóż konto</button>
-                                <button type="submit" className="btn submit mx-2" onSubmit={handleSubmit}>Zaloguj się</button>
+                                <button type="submit" className="btn mx-2">Zaloguj się</button>
                             </div>
                         </form>
                     </Col>
