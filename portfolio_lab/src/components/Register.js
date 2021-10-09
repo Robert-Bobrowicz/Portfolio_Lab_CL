7e8d18c5fb45217bdfbd422af7df09112661ae4e
unknown <r.bobrowicz@live.com>
2021-10-09 08:48:55 +0200

    validation

--- portfolio_lab/src/components/Register.js
+++ portfolio_lab/src/components/Register.js
@@ -96,7 +96,7 @@ const Register = () => {
                                 <div className="loginPasswordRepeat">
                                     <label className="mb-2">Powtórz hasło</label>
                                     <input
-                                        type="password"
+                                        type="text"
                                         className="mb-2"
                                         onChange={handlePasswordRepeat}
                                         value={passwordRepeat}/>
